7dc1cbf3ed676eb01b518c1d3a79b847bd9543d7
Craig Mudzingwa <tanxyrogue@gmail.com>
2021-08-30 21:38:57 +0200

    added site header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <h1>Tanxy's Bot Store</h1>
+            <button className="signup-btn">Sign Up</button>
+        </header>
+    )
+}
+
+export default Header
