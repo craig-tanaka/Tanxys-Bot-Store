01ce83d9c2790f5c2b4b4852780e0dc2cdfd8a64
Craig Mudzingwa <tanxyrogue@gmail.com>
2021-10-14 09:00:49 +0200

    renamed old header to nav and added new header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 const Header = () => {
     return (
-        <header className="header">
-            <h1>Tanxy's Bot Store</h1>
-            <button className="signup-btn">Sign Up</button>
+        <header>
+            <div className="">
+                <h2>Get Yourself The Best Bots in The Bizz</h2>
+                <p>With our bots you don't have to spend every waking hour watching your charts
+                    and countless more analysing the same charts over and over again.
+                    Our bots anaylse everything for you, set your take profits and stop losses.
+                    All you have to do is sit back and reap the proits.
+                    It's trading like it was meant to be.
+                </p>
+            </div>
         </header>
     )
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return (
+        <nav>
+            <h1>Tanxy's Bot Store</h1>
+            <button className="signup-btn">Sign Up</button>
+        </nav>
+    )
+}
+
+export default Nav
