929675058dc3c2eb22afdf78a1717b02fa3d82d6
Craig Mudzingwa <tanxyrogue@gmail.com>
2021-10-14 09:45:49 +0200

    added a little header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <header>
             <img src={ banner } alt="trading" className="banner-img"/>
-            <div className="">
-                <h2>Get Yourself The Best Bots in The Bizz</h2>
-                <p>With our bots you don't have to spend every waking hour watching your charts
+            <div className="banner-text">
+                <h2 className="banner-text-heading">Get Yourself The Best Bots in The Bizz</h2>
+                <p className="banner-text-body">With our bots you don't have to spend every waking hour watching your charts
                     and countless more analysing the same charts over and over again.
                     Our bots anaylse everything for you, set your take profits and stop losses.
                     All you have to do is sit back and reap the proits.
